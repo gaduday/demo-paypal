f5951c55fd70dc3f0e6c6561f18bbe12b889c20c
gaduday <quangtrungnhd@gmail.com>
2021-01-08 11:16:48 +0700

    add form field

--- public/app.js
+++ public/app.js
@@ -4,11 +4,30 @@ paymentButton.addEventListener("click", (e) => {
   e.preventDefault()
   const money = document.getElementById("payment-money").value
   const reason = document.getElementById("info").value
-  const error = document.querySelector("#error")
+  const name = document.getElementById('name').value;
+  const phone = document.getElementById('phone').value;
+  const email = document.getElementById('email').value;
+
+  const errorMoney = document.querySelector("#error-money")
+  const errorEmail = document.querySelector("#error-email")
+  const errorPhone = document.querySelector("#error-phone")
 
   if (money <= 0 || money == "") {
-    error.textContent = "invalid money"
+    errorMoney.textContent = 'invalid money';
     return
+  } else {
+    errorMoney.textContent = '';
+    // return
+  }
+
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!re.test(String(email).toLocaleLowerCase()) || email == "") {
+    errorEmail.textContent = 'invalid email';
+    return;
+  } else {
+    errorEmail.textContent = '';
+    // return
   }
 
   // await fetch('/buy?money=' + money + '&reason=' + reason)
